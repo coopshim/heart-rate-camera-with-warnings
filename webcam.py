9e9cbe926bbe492fcc9ffe4412212a178da75e8b
SEUNGHYEOP SHIM <coopshim@yu.ac.kr>
2025-03-24 23:17:53 +0900

    Update webcam
    self.index

--- webcam.py
+++ webcam.py
@@ -5,18 +5,17 @@ import time
 class Webcam(object):
     def __init__(self):
         #print ("WebCamEngine init")
-        self.dirname = "" #for nothing, just to make 2 inputs the same
+        self.index = 0  # Camera input number
+        self.dirname = "" # for nothing, just to make 2 inputs the same
         self.cap = None
     
     def start(self):
         print("[INFO] Start webcam")
         time.sleep(1) # wait for camera to be ready
-        self.cap = cv2.VideoCapture(0) # 내장 카메라 사용시 0, 외부 웹캠 사용시 1 혹은 다른 숫자 입력
-        '''
+        self.cap = cv2.VideoCapture(self.index)
         # 웹캠 해상도 조절 기능 (주의: 고해상도일수록 불러오는 시간이 증가함.)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # 웹캠 해상도 조절(Width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # 웹캠 해상도 조절(Height)
-        '''
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 웹캠 해상도 조절(Width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 웹캠 해상도 조절(Height)
         self.valid = False
         try:
             resp = self.cap.read()
@@ -43,4 +42,5 @@ class Webcam(object):
         if self.cap is not None:
             self.cap.release()
             print("[INFO] Stop webcam")
+
         
